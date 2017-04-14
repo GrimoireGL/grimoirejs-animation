9eaf1f6060ea864e2dedb94ef1b7030872a51ffa
case2912 <shiroyama@maia.eonet.ne.jp>
2017-04-14 15:50:29 +0900

    [from now] 2017/04/14 15:50:29
    diff --git a/src/Animation/Schema/IAnimationEffectName.ts b/src/Animation/Schema/IAnimationEffectName.ts
    new file mode 100644
    index 0000000..eea5abd
    --- /dev/null
    +++ b/src/Animation/Schema/IAnimationEffectName.ts
    @@ -0,0 +1,6 @@
    +export interface EffectName extends String { }
    +export namespace EffectName {
    +    export const LINEAR: EffectName = "LINEAR";
    +    export const DESCRETE: EffectName = "DESCRETE";
    +    export const BEZIER: EffectName = "BEZIER"
    +}
    \ No newline at end of file

---
+++ src/Animation/Schema/IAnimationEffectName.ts
@@ -0,0 +1,6 @@
+export interface EffectName extends String { }
+export namespace EffectName {
+    export const LINEAR: EffectName = "LINEAR";
+    export const DESCRETE: EffectName = "DESCRETE";
+    export const BEZIER: EffectName = "BEZIER"
+}
